7602853e35829c7dc4dbe1621fda4bd7b9a1be07
Cleberson <clebersonrochaads@gmail.com>
2023-09-04 12:14:22 -0300

    Entendendo migrations, seeders, factorys, models Aulas 19 a 42

--- curso_laravel/app/Http/Controllers/CategoriaController.php
+++ curso_laravel/app/Http/Controllers/CategoriaController.php
@@ -2,22 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Produto;
 
-class ProdutoController extends Controller
+class CategoriaController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //return "index";
-        $produtos = Produto::all();
-        //return dd($produtos);
-
-        return view('site.home', compact('produtos'));
+        //
     }
 
     /**
@@ -39,7 +35,7 @@ class ProdutoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Categoria $categoria)
     {
         //
     }
@@ -47,7 +43,7 @@ class ProdutoController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Categoria $categoria)
     {
         //
     }
@@ -55,7 +51,7 @@ class ProdutoController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Categoria $categoria)
     {
         //
     }
@@ -63,7 +59,7 @@ class ProdutoController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Categoria $categoria)
     {
         //
     }
